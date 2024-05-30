7e9017aa3f6828d297d750c7624138f1c9991139
방승호 <deme1323@gmail.com>
2024-05-31 02:42:18 +0900

    update

--- client/src/Component/Main.js
+++ client/src/Component/Main.js
@@ -47,7 +47,7 @@ function Main() {
                 </div>
                 <div className="item">
                     FinalFantasy Tifa
-                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1717083070333A5.JPEG" alt="lifeline" className="image" />
+                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1717090404089A1.JPEG" alt="lifeline" className="image" />
                     <div className="audio-container">
                         Generated
                         <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530152634.wav" controls></audio>
@@ -55,9 +55,6 @@ function Main() {
                         <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530162148.wav" controls></audio>
                     </div>
                 </div>
-            </div>
-            <hr/>
-            <div className="container">
                 <div className="item">
                     RDR2 Arthur Morgan
                     <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1716566157368A23.JPEG" alt="lifeline" className="image" />
@@ -68,16 +65,9 @@ function Main() {
                         <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530154948.wav" controls></audio>
                     </div>
                 </div>
-                <div className="item">
-                    Professor X
-                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1717086885603A16.JPEG" alt="Thrall" className="image" />
-                    <div className="audio-container">
-                        Generated
-                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530163040.wav" controls></audio>
-                        Generated2
-                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530163141.wav" controls></audio>
-                    </div>
-                </div>
+            </div>
+            <hr/>
+            <div className="container">
                 <div className="item">
                     Apex Legend Lifeline
                     <img src="https://kr.object.ncloudstorage.com/inu-attention/main/lifeline_img.jpg" alt="lifeline" className="image" />
@@ -86,6 +76,26 @@ function Main() {
                         <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/lifeline_gene1.wav" controls></audio>
                         Generated2
                         <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/lifeline_gene2.wav" controls></audio>
+                    </div>
+                </div>
+                <div className="item">
+                    Jack
+                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1717089046582A1.avifJPEG" alt="lifeline" className="image" />
+                    <div className="audio-container">
+                        Generated
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530170611.wav" controls></audio>
+                        Generated2
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530170817.wav" controls></audio>
+                    </div>
+                </div>
+                <div className="item">
+                    Professor X
+                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1717086885603A16.JPEG" alt="Thrall" className="image" />
+                    <div className="audio-container">
+                        Generated
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530163040.wav" controls></audio>
+                        Generated2
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530163141.wav" controls></audio>
                     </div>
                 </div>
             </div>
