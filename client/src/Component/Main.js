11714e13e49c308a33c69ae96bfe8f32f205dd1c
방승호 <deme1323@gmail.com>
2024-05-31 02:02:29 +0900

    update

--- client/src/Component/Main.js
+++ client/src/Component/Main.js
@@ -20,9 +20,11 @@ function Main() {
             <h2>Image2Voice</h2>
             <br/>
             <br/>
-            <p>Image2Voice는 <br/>이미지를 분석하여 사용자 개인만의 유일한 음성을 생성하는<br/>서비스를 제공합니다.</p>
-
+            <p>Image2Voice는 <br/>이미지를 분석하여 사용자 개인만의 유일한 음성을 생성하는<br/>서비스를 제공합니다.</p><br/><br/><br/>
+            <h1>Preview</h1>
+            <hr className="divider"/>
             <div className='container'>
+
                 <div className="item">
                     Apex Legend Fuse
                     <img src="https://kr.object.ncloudstorage.com/inu-attention/main/fuse_img.jpg" alt="Fuse" className="image" />
@@ -31,6 +33,39 @@ function Main() {
                         <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/fuse_gene.wav" controls></audio>
                         Original
                         <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/fuse_ori.wav" controls></audio>
+                    </div>
+                </div>
+                <div className="item">
+                    WOW Thrall
+                    <img src="https://kr.object.ncloudstorage.com/inu-attention/main/thrall_img.jpg" alt="Thrall" className="image" />
+                    <div className="audio-container">
+                        Generated
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/thrall_gene.wav" controls></audio>
+                        Original
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/thrall_ori.ogg" controls></audio>
+                    </div>
+                </div>
+                <div className="item">
+                    FinalFantasy Tifa
+                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1717083070333A5.JPEG" alt="lifeline" className="image" />
+                    <div className="audio-container">
+                        Generated
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530152634.wav" controls></audio>
+                        Generated2
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530162148.wav" controls></audio>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div className="container">
+                <div className="item">
+                    RDR2 Arthur Morgan
+                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1716566157368A23.JPEG" alt="lifeline" className="image" />
+                    <div className="audio-container">
+                        Generated
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530154743.wav" controls></audio>
+                        Generated2
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530154948.wav" controls></audio>
                     </div>
                 </div>
                 <div className="item">
@@ -44,27 +79,19 @@ function Main() {
                     </div>
                 </div>
                 <div className="item">
-                    FinalFantasy Tifa
-                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1717083070333A5.JPEG" alt="lifeline" className="image" />
+                    Apex Legend Lifeline
+                    <img src="https://kr.object.ncloudstorage.com/inu-attention/main/lifeline_img.jpg" alt="lifeline" className="image" />
                     <div className="audio-container">
                         Generated
-                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530152634.wav" controls></audio>
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/lifeline_gene1.wav" controls></audio>
                         Generated2
-                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530162148.wav" controls></audio>
-                    </div>
-                </div>
-                <div className="item">
-                    RDR2 Arthur Morgan
-                    <img src="https://inu-attention.kr.object.ncloudstorage.com/style/1716566157368A23.JPEG" alt="lifeline" className="image" />
-                    <div className="audio-container">
-                        Generated
-                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530154743.wav" controls></audio>
-                        Generated2
-                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/TTS/0530154948.wav" controls></audio>
+                        <audio src="https://kr.object.ncloudstorage.com/inu-attention/main/lifeline_gene2.wav" controls></audio>
                     </div>
                 </div>
             </div>
+            <hr className="divider"/>
         </div>
+
 
         </>
     );
