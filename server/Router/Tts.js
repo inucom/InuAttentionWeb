9012edc95d8d41108464eff3dc89052b5bd356ae
방승호 <deme1323@gmail.com>
2024-05-31 00:54:46 +0900

    updats

--- server/Router/Tts.js
+++ server/Router/Tts.js
@@ -5,96 +5,47 @@ const {Tts} = require("../Model/Tts");
 const {Manage} = require("../Model/Manage");
 const axios = require("axios");
 
-
 router.post("/submit", async (req, res) => {
     try {
         const { text, styleId } = req.body;
 
         const newTts = new Tts({ text, styleId });
 
-        res.status(200).json({ success: true });
+        const [style, manage] = await Promise.all([
+            Style.findOneAndUpdate(
+                { _id: styleId },
+                { $inc: { ttsNum: 1 } },
+                { new: true }
+            ).exec(),
+            Manage.findOne({ identifier: 1 }).exec()
+        ]);
 
-        (async () => {
-            try {
-                const [style, manage] = await Promise.all([
-                    Style.findOneAndUpdate(
-                        { _id: styleId },
-                        { $inc: { ttsNum: 1 } },
-                        { new: true }
-                    ).exec(),
-                    Manage.findOne({ identifier: 1 }).exec()
-                ]);
+        newTts.ttsNum = style.ttsNum;
 
-                newTts.ttsNum = style.ttsNum;
+        const requestData = {
+            data: [{
+                text,
+                auto: style.auto,
+                diff: style.diffusion,
+                statusCode: 1
+            }]
+        };
 
-                const requestData = {
-                    data: [{
-                        text,
-                        auto: style.auto,
-                        diff: style.diffusion,
-                        statusCode: 1
-                    }]
-                };
+        const response = await axios.post(manage.URL, requestData);
 
-                const response = await axios.post(manage.URL, requestData);
+        const audioFileName = response.data.data[0].audio;
+        const baseURL = "https://kr.object.ncloudstorage.com/inu-attention/TTS/";
 
-                const audioFileName = response.data.data[0].audio;
-                const baseURL = "https://kr.object.ncloudstorage.com/inu-attention/TTS/";
+        newTts.audio = `${baseURL}${audioFileName}`;
 
-                newTts.audio = `${baseURL}${audioFileName}`;
+        await newTts.save();
 
-                await newTts.save();
-            } catch (err) {
-                console.error(err);
-            }
-        })();
+        return res.status(200).json({ success: true });
     } catch (err) {
-        console.error(err);
-        res.status(400).json({ success: false });
+        console.error("Error processing TTS request:", err);
+        return res.status(400).json({ success: false});
     }
 });
-
-// router.post("/submit", async (req, res) => {
-//     try {
-//         const { text, styleId } = req.body;
-//
-//         const newTts = new Tts({ text, styleId });
-//
-//         const [style, manage] = await Promise.all([
-//             Style.findOneAndUpdate(
-//                 { _id: styleId },
-//                 { $inc: { ttsNum: 1 } },
-//                 { new: true }
-//             ).exec(),
-//             Manage.findOne({ identifier: 1 }).exec()
-//         ]);
-//
-//         newTts.ttsNum = style.ttsNum;
-//
-//         const requestData = {
-//             data: [{
-//                 text,
-//                 auto: style.auto,
-//                 diff: style.diffusion,
-//                 statusCode: 1
-//             }]
-//         };
-//
-//         const response = await axios.post(manage.URL, requestData);
-//
-//         const audioFileName = response.data.data[0].audio;
-//         const baseURL = "https://kr.object.ncloudstorage.com/inu-attention/TTS/";
-//
-//         newTts.audio = `${baseURL}${audioFileName}`;
-//
-//         await newTts.save();
-//
-//         return res.status(200).json({ success: true });
-//     } catch (err) {
-//         console.error("Error processing TTS request:", err);
-//         return res.status(400).json({ success: false});
-//     }
-// });
 
 // router.post("/submit", async (req, res) => {
 //     try {
